ff2b08c1d54348cb10649132737bf684081d55c0
Thomas <kjaerulf1988@hotmail.com>
2026-05-14 13:48:58 +0200

    fix regler page

---
+++ src/Regler.jsx
@@ -0,0 +1,13 @@
+export default function Regler() {
+  return (
+    <div
+      style={{
+        color: "white",
+        padding: "50px",
+        fontSize: "30px"
+      }}
+    >
+      VM Tips Regler virker 🔥
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Leaderboard from "./Leaderboard";
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import App from "./App";
 import AdminPanel from "./AdminPanel";
+import Leaderboard from "./Leaderboard";
+import Regler from "./Regler";
 
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/admin" element={<AdminPanel />} />
+        <Route
+          path="/"
+          element={<App />}
+        />
+
+        <Route
+          path="/admin"
+          element={<AdminPanel />}
+        />
+
+        <Route
+          path="/leaderboard"
+          element={<Leaderboard />}
+        />
+
+        <Route
+          path="/regler"
+          element={<Regler />}
+        />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
